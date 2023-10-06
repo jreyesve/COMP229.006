ecb00fc4610c28ceea16b6796401148a8e38c268
jreyesve <jreyesve@my.centennialcollege.ca>
2023-10-06 18:09:42 -0400

    commit 1
    -Add new pages( Services, About, Contact)
    -Delete Products  View

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,19 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home' , { title: 'Express' });
+  res.render('home' , { title: 'Home' });
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('home', { title: 'About' });
+  res.render('about', { title: 'About me' });
 });
 
-router.get('/products', function(req, res, next) {
-  res.render('products', 
-  { 
-    product: 'Products',
-    title: 'Phone'
-  });
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', {title: 'Projects'});
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', {title: 'Services'});
+});
+
+/* GET contact me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {title: 'Contact me'});
 });
 
 module.exports = router;
