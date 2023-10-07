8d548f4af7ee23a97ce81a3b157090159c17ffb7
jreyesve <jreyesve@my.centennialcollege.ca>
2023-10-07 12:05:45 -0400

    commit 7
    Web site Finished
    -Fixed css and ejs variables

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,22 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home' , { title: 'Home' }); 
+  res.render('home' , { title: 'Home',
+                        welcome: 'Welcome',
+                        mission: 'Mission' }); 
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About me' });
+  res.render('about', { title: 'About me',
+                        name: 'Julio Reyes',
+                        });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', {title: 'Projects'});
+  res.render('projects', {title: 'Projects',
+                          project1: 'Movie Poster Dashboard',
+                          project2: 'Pizza Orders Application',
+                          project3: 'Web Site'});
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', {title: 'Services'});
+  res.render('services', {title: 'Services',
+                          service1: 'Web Development',
+                          service2: 'Mobile App Development',
+                          service3: 'Software Development',
+                          service4: 'Testing and QA',
+                          service5: 'UI/UX Design',
+                          service6: 'Data Analytics',
+                          service7: 'Cibersecurity',
+                          service8: 'Cloud Services',
+                          service9: 'Digital Transformation'});
 });
 
 /* GET contact me page. */
